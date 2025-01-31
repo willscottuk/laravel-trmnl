436ff330638c30faa670b43c3cc0886a50a64812
bnussbau <8931007+bnussbau@users.noreply.github.com>
2025-01-31 16:04:39 +0000

    Fix styling

--- src/Jobs/UpdateScreenContentJob.php
+++ src/Jobs/UpdateScreenContentJob.php
@@ -13,16 +13,14 @@ class UpdateScreenContentJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public function __construct(public array $content)
-    {
-    }
+    public function __construct(public array $content) {}
 
     public function handle(): void
     {
         if (config('trmnl.data_strategy') !== 'webhook') {
             throw new \Exception('Aborting Job. Data strategy is not webhook. Set TRMNL_DATA_STRATEGY=webhook in your .env.');
         }
-        if (config('trmnl.webhook_url') === null){
+        if (config('trmnl.webhook_url') === null) {
             throw new \Exception('Aborting Job. Webhook URL not set. Set TRMNL_WEBHOOK_URL in your .env.');
         }
 
